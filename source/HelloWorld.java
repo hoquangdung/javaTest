5ab1a7fbf6c2b280d8f0b632fa04804042025d06
user <user@linux-555l.site>
2016-05-02 14:42:14 -0400

    modified java

--- source/HelloWorld.java
+++ source/HelloWorld.java
@@ -3,6 +3,10 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		int x=5;
+
+		if (x>5) {
+			System.out.println("Hello World!");
+		}
 	}
 }
